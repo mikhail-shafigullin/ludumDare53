980e95b78b3845b4107e0542a2bf922924263791
nn <86272232+loop-nop@users.noreply.github.com>
2023-04-29 08:24:03 +0000

    Animation test

--- main_menu.gd
+++ main_menu.gd
@@ -1,12 +1,20 @@
 extends Control
-@onready var button = $Button
+@onready var exit_button = $VBoxContainer/Exit
+@onready var test_button = $VBoxContainer/Test
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	button.pressed.connect(func(): get_tree().quit())
+	exit_button.pressed.connect(func(): get_tree().quit())
+	test_button.pressed.connect(nn_test)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func nn_test():
+	var nn_scene = load("res://test/nn_test.tscn").instantiate()
+	get_tree().get_root().add_child(nn_scene)
+	
+	queue_free()
