6841e7efcb252cc54dd6195309a5184a98d75b89
nn <86272232+loop-nop@users.noreply.github.com>
2023-04-30 07:54:29 +0000

    testing no more

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,6 +1,5 @@
 extends Control
 @onready var exit_button = $VBoxContainer/Exit
-@onready var test_button = $VBoxContainer/Test
 
 var playerResource = preload("res://characters/player/player.tscn")
 var transitionScreen = preload("res://ui/TransitionScreen.tscn")
@@ -9,14 +8,6 @@ var transitionScreen = preload("res://ui/TransitionScreen.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	exit_button.pressed.connect(func(): get_tree().quit())
-	test_button.pressed.connect(nn_test)
-
-
-func nn_test():
-	State.player = playerResource.instantiate()
-	get_tree().root.add_child(State.player)
-	get_tree().change_scene_to_file("res://test/nn_test.tscn")
-
 
 func _on_gameplay_test_pressed():
 	State.player = playerResource.instantiate()
