bf9970da81875264aef86575403566315008c01b
AltTabber <mikeice414@gmail.com>
2023-04-30 16:27:33 +0400

    add debugMenu

--- state.gd
+++ state.gd
@@ -1,8 +1,15 @@
 extends Node
 
-enum LocationState {STATE_MAIN_ROOM, STATE_HALLWAY, STATE_KITCHEN}
+enum LocationState {
+	STATE_MAIN_ROOM, 
+	STATE_HALLWAY, 
+	STATE_KITCHEN, 
+	STATE_LONG_ROOM,
+	STATE_DEBUG
+}
 
 var player_is_busy := false
 var player: Node2D;
 var location: LocationState;
 var sceneManager: Node2D;
+var day: int = 0;
---
+++ ui/debugMenu.gd
@@ -0,0 +1,62 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func teleportTo1Day():
+	State.day = 1;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+	
+func teleportTo2Day():
+	State.day = 2;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+
+func teleportTo3Day():
+	State.day = 3;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+
+func teleportTo4Day():
+	State.day = 4;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+	
+func teleportTo5Day():
+	State.day = 5;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+	
+func teleportTo6Day():
+	State.day = 6;
+	State.location = State.LocationState.STATE_DEBUG;
+	State.sceneManager.transtionTo("res://levels/main_room_scene.tscn");
+
+func _on_1_day_button_pressed():
+	teleportTo1Day();
+
+
+func _on_2_day_button_pressed():
+	teleportTo2Day();
+
+
+func _on_3_day_button_pressed():
+	teleportTo3Day();
+
+
+func _on_4_day_button_pressed():
+	teleportTo4Day();
+
+func _on_5_day_button_pressed():
+	teleportTo5Day();
+
+func _on_6_day_button_pressed():
+	teleportTo6Day();
