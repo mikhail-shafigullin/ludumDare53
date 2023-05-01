667b0cb8e36c134ed4814198761788c9278f2bff
nn <86272232+loop-nop@users.noreply.github.com>
2023-05-01 07:18:36 +0000

    audio volume

---
+++ audio/sound_mixer.gd
@@ -0,0 +1,13 @@
+extends Control
+@onready var sound_slider = $SoundSlider
+@onready var sound_button = $SoundButton
+
+
+func _on_sound_button_pressed():
+	sound_slider.visible = sound_button.button_pressed
+
+
+
+func _on_sound_slider_drag_ended(value_changed):
+	if value_changed:
+		AudioServer.set_bus_volume_db(0, sound_slider.value)
